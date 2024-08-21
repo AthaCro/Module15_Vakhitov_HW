7ba8d71479dffdf45f069e2c3e68b4a1c50cf728
AthaCro <lordoftimuria@yandex.ru>
2024-08-21 21:56:48 +0300

    rework 1

--- Module15_Vakhitov_HW.cpp
+++ Module15_Vakhitov_HW.cpp
@@ -2,31 +2,37 @@
 
 const int n = 20;
 
-void OddNumbers()
+void OddNumbers(int arg_1, bool arg_2)
 {
-
-	for (int i = 0; i < ::n; ++i) // вывод нечетных
+	if (arg_2)
 	{
-			if (i % 2)
+		for (int i = 1; i < ::n; ++i) // вывод нечетных
+		{
+				if (i % 2)
+				std::cout << i << "\n";
+
+		}
+	}
+		
+	else
+	{
+		for (int i = 2; i < ::n; i += 2) // вывод четных
+		{
+
 			std::cout << i << "\n";
+
+		}
 	}
 }
 
-void EvenNumbers()
-{
-	for (int i = 0; i < ::n; i += 2) // вывод четных
-	{
-
-		std::cout << i << "\n";
-
-	}
-}
 
 int main()
 {
-	OddNumbers();
+	
+	OddNumbers(n, true);
 	std::cout << "\n";
-	EvenNumbers();
+	OddNumbers(n, false);
+	
 }
 
 
